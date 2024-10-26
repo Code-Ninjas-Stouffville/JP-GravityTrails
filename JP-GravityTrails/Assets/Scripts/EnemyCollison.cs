f029c8702ddaaa27acbc1e442a1de71241f2f62a
Code-Ninjas-Stouffville <157077338+Code-Ninjas-Stouffville@users.noreply.github.com>
2024-10-26 12:01:14 -0400

    do step 54
    gs egdvxb

---
+++ JP-GravityTrails/Assets/Scripts/EnemyCollison.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyCollison : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
